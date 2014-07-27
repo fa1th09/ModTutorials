c6c136e0105a69d4a0223ef91f14b6d870dde156
fa1th09 <kizzy_b@hotmail.co.uk>
2014-07-27 03:12:25 +0100

    Video 5

---
+++ src/main/java/com/fa1th/modtutorials/reference/Reference.java
@@ -0,0 +1,8 @@
+package com.fa1th.modtutorials.reference;
+
+public class Reference
+{
+    public static final String MOD_ID = "ModTutorials";
+    public static final String MOD_NAME = "Mod Tutorials";
+    public static final String VERSION = "1.7.10-1.0";
+}
